037115af0563ff96e2e1d277437a3a96101da16f
omegascorp <omegascorp@gmail.com>
2019-07-01 00:06:43 +0400

    fix: add response to use subscription

--- src/use-subscription.ts
+++ src/use-subscription.ts
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import { IStore } from "rex-store/typescript/interfaces";
 
-export function useSubscription(store: IStore<any>, eventName: string, eventListener: () => void) {
+export function useSubscription(
+  store: IStore<any>,
+  eventName: string,
+  eventListener: (result: any) => void,
+) {
   useEffect(() => {
     store.on(eventName, eventListener);
     return () => {
