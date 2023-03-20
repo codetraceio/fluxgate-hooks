fd25a5dd4534e1d0d6a841911406584a0d6a1da0
omegascorp <omegascorp@gmail.com>
2023-03-20 10:15:33 +0400

    add dependency list to useSubscription

--- src/useSubscription.ts
+++ src/useSubscription.ts
@@ -1,15 +1,16 @@
-import { useEffect } from "react";
+import { DependencyList, useEffect } from "react";
 import { Store } from "fluxgate/typescript/interfaces";
 
 export function useSubscription(
   store: Store<any>,
   eventName: string,
   eventListener: (result: any) => void,
+  deps: DependencyList = [],
 ) {
   useEffect(() => {
     store.on(eventName, eventListener);
     return () => {
       store.off(eventName, eventListener);
     };
-  }, []);
+  }, deps);
 }
