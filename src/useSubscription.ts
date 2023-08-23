7d013cdccf3872cd7fcb94a117529d9295b88dcb
omegascorp <omegascorp@gmail.com>
2023-08-23 15:44:37 +0400

    add support of store name to useSubscription and useSubscriptionIf

--- src/useSubscription.ts
+++ src/useSubscription.ts
@@ -1,12 +1,16 @@
-import { DependencyList, useEffect } from "react";
+import { DependencyList, useContext, useEffect } from "react";
 import { Store } from "fluxgate/typescript/interfaces";
+import { StoreContent } from "./storeContext";
 
-export function useSubscription(
-  store: Store<any>,
+export function useSubscription<T>(
+  storeName: string | Store<T>,
   eventName: string,
   eventListener: (result: any) => void,
   deps: DependencyList = [],
 ) {
+  const storeContext = useContext(StoreContent);
+  const store = typeof storeName === "string" ? storeContext.stores[storeName] : storeName;
+
   useEffect(() => {
     store.on(eventName, eventListener);
     return () => {
--- src/useSubscriptionIf.ts
+++ src/useSubscriptionIf.ts
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { Store } from "fluxgate/typescript/interfaces";
+import { StoreContent } from "./storeContext";
 
-export function useSubscriptionIf(
-  store: Store<any>,
+export function useSubscriptionIf<T>(
+  storeName: string | Store<T>,
   condition: boolean,
   eventName: string,
   eventListener: (result: any) => void,
 ) {
+  const storeContext = useContext(StoreContent);
+  const store = typeof storeName === "string" ? storeContext.stores[storeName] : storeName;
+
   useEffect(() => {
     if (condition) {
       store.on(eventName, eventListener);
